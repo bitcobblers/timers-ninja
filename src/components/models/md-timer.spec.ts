f166bad9cb0a1791580fbc3bc136cb2f7b9f4a23
Sergei Golos <sergei.golos@gmail.com>
2024-03-22 16:24:34 -0400

    lexer is working to parse time segments

--- src/components/models/md-timer.spec.ts
+++ src/components/models/md-timer.spec.ts
@@ -28,9 +28,9 @@ const WhiteSpace = createToken({
 });
 
 const Identifier = createToken({ name: "Identifier", pattern: /[a-zA-Z]\w*/ });
-const Integer = createToken({ name: "Integer", pattern: /0|[1-9]\d*/ });
+const Integer = createToken({ name: "Integer", pattern: /\d+/ });
 const Comma = createToken({ name: "Comma", pattern: /,/ });
-const Collon = createToken({ name: "Collon", pattern: /:/ });
+const Colon = createToken({ name: "Colon", pattern: /:/ });
 
 const CountDirection = createToken({ name: "CountDirection", pattern: Lexer.NA });
 const Minus = createToken({ name: "Minus", pattern: /-/, categories: CountDirection });
@@ -44,7 +44,8 @@ let allTokens = [
     // "keywords" appear before the Identifier
     LabelOpen,
     LabelClose,
-    Collon,
+    Colon,
+    CountDirection,
     Minus,
     Plus,
     Comma,
@@ -54,7 +55,7 @@ let allTokens = [
   ];  
 
   import { CstParser } from "chevrotain";
-import { as } from "vitest/dist/reporters-5f784f42";
+import { Result } from "postcss";
 
   // ----------------- parser -----------------
 class MdTimerParse extends CstParser {
@@ -62,60 +63,163 @@ class MdTimerParse extends CstParser {
         super(allTokens);
         const $ = this as any;
 
-        $.RULE("timer", () => {
+        // $.RULE("timer", () => {
+        //     $.SUBRULE($.timerDirection);
+        //     $.SUBRULE($.timeSpan);
+        //     $.OPTION(() => {
+        //         $.SUBRULE($.labels);
+        //     });
+        //     $.OPTION(() => {
+        //         $.SUBRULE($.multiple);
+        //     });
+        // });
+
+        $.RULE("timerExpression", () => {
             $.SUBRULE($.timerDirection);
-            $.SUBRULE($.timeSpan);
+            $.SUBRULE($.timeSpanExpression)
+        })
+
+        $.RULE("timerDirection", () => {
             $.OPTION(() => {
-                $.SUBRULE($.labels);
+                $.CONSUME(CountDirection);            
             });
-            $.OPTION(() => {
-                $.SUBRULE($.multiple);
+        })
+
+        $.RULE("timeSpanExpression", () => {
+            $.SUBRULE($.timeSpan)
+        });
+
+        $.RULE("timeSpan", () => {
+            $.SUBRULE($.numericExpression, { LABEL: "lhs"});
+            $.MANY(() => {
+                $.CONSUME(Colon);
+                $.SUBRULE2($.timeSpan)                    
             });
         });
 
-        $.RULE("timerDirection", () => {
-            $.OR([
-                { ALT: () => $.OPTION(() => {
-                    $.CONSUME(Plus)
-                }) },
-                { ALT: () => $.CONSUME(Minus) },
-            ]);
+        $.RULE("numericExpression", ()=> {
+            $.CONSUME(Integer)
         })
+        // $.RULE("labels", () => {})
 
-        $.RULE("additionExpression", () => {
-            $.SUBRULE($.multiplicationExpression, { LABEL: "lhs" });
-            $.MANY(() => {
-              // consuming 'AdditionOperator' will consume either Plus or Minus as they are subclasses of AdditionOperator
-              $.CONSUME(AdditionOperator);
-              //  the index "2" in SUBRULE2 is needed to identify the unique position in the grammar during runtime
-              $.SUBRULE2($.multiplicationExpression, { LABEL: "rhs" });
-            });
-            });
-        
+        // $.RULE("parenthesisExpression", () => {
+        //     $.CONSUME(LabelOpen);
+        //     $.SUBRULE($.expression);
+        //     $.CONSUME(LabelClose);
+        //     });
 
 
-        $.RULE("timeSpan", () => {})
-        $.RULE("labels", () => {})
-
-        $.RULE("parenthesisExpression", () => {
-            $.CONSUME(LabelOpen);
-            $.SUBRULE($.expression);
-            $.CONSUME(LabelClose);
-            });
-
-
-        $.RULE("multiple", () => {})
+        // $.RULE("multiple", () => {})
         $.performSelfAnalysis();
     }
 
 }
 
+const parser = new MdTimerParse() as any;
+
+// ----------------- Interpreter -----------------
+const BaseCstVisitor = parser.getBaseCstVisitorConstructor();
+
+type TimeSegment = {
+    current?: number;
+    next?: TimeSegment;
+}
+class MdTimerInterpreter extends BaseCstVisitor {
+    constructor() {
+      super();
+      // This helper will detect any missing or redundant methods on this visitor
+            
+      
+      this.validateVisitor();
+    }
+
+    timerExpression(ctx: any) {
+        let composer = new SegmentComposer();
+        return{ 
+            direction: ctx.timerDirection ? this.visit(ctx.timerDirection) : "countup", 
+            timer: composer.compose(this.visit(ctx.timeSpanExpression))
+        }
+    }
+
+    timerDirection(ctx:any) {                
+        return ctx.CountDirection && ctx.CountDirection[0].tokenType == Minus 
+            ? "countdown"
+            : "countup";
+    }
+
+    timeSpanExpression(ctx: any) {        
+        return this.visit(ctx.timeSpan);
+    }
+
+    timeSpan(cxt:any){                        
+        let result:TimeSegment = {
+           current : this.visit(cxt.lhs)
+        };        
+        if(cxt.timeSpan){
+            result.next = this.visit(cxt.timeSpan)
+        }                            
+        return result;
+    }
+
+    numericExpression(ctx: any) {  
+        const value = ctx.Integer[0].image;
+        return Number(value);
+    }
+}
+
+class SegmentComposer {
+    
+    compose(item:TimeSegment): Date{ 
+        var result = this.c(item);
+        while(result.length < 6) {
+            result.push(0);
+        }
+        result = result.reverse();
+        return new Date(result[0], result[1], result[2], result[3], result[4], result[5], 0)
+    }
+    
+    private c(item: TimeSegment): number[] {
+        if (!item.next) {
+            return [item.current||0];
+        }
+        const result = this.c(item.next);
+        result?.push(item.current || 0);
+        return result;
+    }
+}
 
 test(`ExpectingTokenToWork`, async () => {
     let SelectLexer = new Lexer(allTokens);    
-    let inputText = "-1:00[pushup]";
-    let lexingResult = SelectLexer.tokenize(inputText);
+    let inputText = "3:10:00";
+    let { tokens } = SelectLexer.tokenize(inputText);
+    parser.input = tokens;
+    let cst = parser.timerExpression();            
+    let visitor = new MdTimerInterpreter();
+    let result = visitor.visit(cst)
+    
+    console.log(result);
 
-    console.log(lexingResult.tokens);
-    expect(lexingResult.tokens.length).toBe(8);
+  });
+
+  test(`ExpectingTokenToWorkNegative`, async () => {
+    let SelectLexer = new Lexer(allTokens);    
+    let inputText = "-3:10:00";
+    let { tokens } = SelectLexer.tokenize(inputText);
+    parser.input = tokens;
+    let cst = parser.timerExpression();            
+    let visitor = new MdTimerInterpreter();
+    let result = visitor.visit(cst)
+    
+    console.log(result);
+
+  });
+
+
+  test(`Composer can Build A Time From the value.`, () => {
+    var item: TimeSegment = { current: 3, next: { current: 10, next: { current: 0 } } };
+    var composer = new SegmentComposer();
+    var result = composer.compose(item);    
+
+    console.log(result);
+    expect(result).toBeTruthy();
   });
