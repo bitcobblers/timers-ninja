11989258b575c6955c23ab0dc61e23bc29d9b951
Sergei Golos <sergei.golos@gmail.com>
2024-09-14 23:32:46 -0400

    started loding the SSG pages

--- src/routes/timers/[slug]/index.tsx
+++ src/routes/timers/[slug]/index.tsx
@@ -5,6 +5,7 @@ import {
   type PathParams,
 } from "@builder.io/qwik-city";
 import { isDev } from "@builder.io/qwik/build";
+import TimerPage from "~/components/timer-page/timer-page";
 
 const metaGlobComponents: Record<string, any> = import.meta.glob("/timers/*", {
   import: "default",
@@ -12,25 +13,41 @@ const metaGlobComponents: Record<string, any> = import.meta.glob("/timers/*", {
   eager: isDev ? false : true,
 });
 export default component$(() => {
-  const { params } = useLocation();
-  return <p>Example: {params.slug}</p>;
+  const { params } = useLocation();  
+  return <TimerPage init={params.seed} title={params.title} />;
 });
 
 export const onStaticGenerate: StaticGenerateHandler = async () => {
   // example of loading params for this use case
   // every implementation will be different
   const args = [] as PathParams[];
+  const titleMatch = new RegExp("# (.*)");
+  const codeMatch = new RegExp("```clock([\\s\\S]*?)```", "g");
   for (const i in metaGlobComponents) {
-    const item = metaGlobComponents[i];    
-    const page = {
-      slug: i.replace("/timers/", "").replace(".md", ""),
-      value: isDev
-        ? await item()
-        : // We need to call `await metaGlobComponents[componentPath]()` in development as it is `eager:false`
-          item,
+    const item = metaGlobComponents[i];        
+    const page = isDev
+      ? await item()
+      : // We need to call `await metaGlobComponents[componentPath]()` in development as it is `eager:false`
+        item;
       // We need to directly access the `metaGlobComponents[componentPath]` expression in preview/production as it is `eager:true`
-    };    
-    args.push(page);
+    
+    const title = titleMatch.exec(page.value);
+    if (title?.groups) {
+      console.log(title.groups[1]);
+    }
+
+    const seed = codeMatch.exec(page.value);
+    if (seed?.groups) {
+      console.log(seed.groups[1]);
+    }
+
+    args.push({
+      slug: i.replace("/timers/", "").replace(".md", ""),
+      seed: seed?.groups![1] || "",
+      title: title?.groups![1] || "",  
+   });
+
+    console.log(page);
   }
   return {
     params: args,
