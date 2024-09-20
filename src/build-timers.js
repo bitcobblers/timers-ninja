a9456254341d9e6a6c44566f425f315f4321b906
Sergei Golos <sergei.golos@gmail.com>
2024-09-20 16:42:25 -0400

    generated the json file before build.

---
+++ src/build-timers.js
@@ -0,0 +1,33 @@
+import matter from 'gray-matter';
+import fs from 'fs';
+import path from 'path';
+
+const pages = [];
+const timersfile = "./src/timers.json";
+const directoryPath = "./timers"
+fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return; 
+    }
+    
+    for (let i = 0; i < files.length; i++) {
+      const slug = files[i].replace(".md", "");
+      const filePath = path.resolve(`timers/${slug}.md`);
+      const file = fs.readFileSync(filePath, 'utf8');
+      const { content } = matter(file);  
+      const titleMatch = new RegExp("# (.*)", "g");
+      const codeMatch = new RegExp("```clock([\\s\\S]*?)```", "g");
+      const title = titleMatch.exec(content);
+      const clock = codeMatch.exec(content);        
+      
+      pages.push({      
+        slug,
+        title: (title[1] || "Unknown"),
+        seed: (clock[1] || ""),
+        raw: content,      
+      })
+    }
+    console.log(pages);
+    fs.writeFileSync(timersfile, JSON.stringify(pages))
+});
