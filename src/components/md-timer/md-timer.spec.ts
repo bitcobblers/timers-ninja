d0dbfc13016fdcfb1e8098e92253039c28ab28c2
Sergei Golos <sergei.golos@gmail.com>
2024-09-26 08:40:06 -0400

    validated time token works with unit test

--- src/components/md-timer/md-timer.spec.ts
+++ src/components/md-timer/md-timer.spec.ts
@@ -5,7 +5,7 @@ test(`parsedDirectionUpDefault`, async () => {
     const runtime = new MdTimerRuntime();
     const { outcome } = runtime.read(":11");    
     
-    expect(outcome).not.toBeNull();    
+    expect(outcome[0].metrics[0].value).not.toBeNull();    
 });
  
 // test(`parsedDirectionUpExplicit`, async () => {    
@@ -31,73 +31,73 @@ test(`parsedDirectionUpDefault`, async () => {
 //     expect(timer).toBe(11);    
 // });
 
-// test(`parsedMinutes`, async () => {    
-//     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read("11:00");
-//     const timer = outcome[0].timer as number
+ test(`parsedMinutes`, async () => {    
+     const runtime = new MdTimerRuntime();
+     const { outcome } = runtime.read("11:00");
+     const timer = outcome[0].metrics[0].value as number
+   
+     expect(timer).toBe(11 * 60);    
+});
+
+test(`parsedHours`, async () => {    
+     const runtime = new MdTimerRuntime();
+     const { outcome } = runtime.read("11:00:00");
+     const timer = outcome[0].metrics[0].value as number
+   
+     expect(timer).toBe(11 * 60 * 60);    
+ });
+
+ test(`parsedDays`, async () => {    
+     const runtime = new MdTimerRuntime();
+     const { outcome } = runtime.read("11:00:00:00");
+     const timer = outcome[0].metrics[0].value as number
+     expect(timer).toBe(11 * 60 * 60 * 24);
+ });
+
+
+test(`parseMultipleLines`, async () => {    
+     const runtime = new MdTimerRuntime();
+     const { outcome } = runtime.read(`:11\r\n-:22`);
+
+     expect(outcome[0].metrics[0].value).toBe(11);
+     expect(outcome[1].metrics[0].value).toBe(22);    
+});
+
+
+
+test(`parseMultipleLinesInGroup`, async () => {    
+     const runtime = new MdTimerRuntime();
+     const { outcome } = runtime.read(`[:11\r\n-:22]`);    
+     expect(outcome[0].metrics[0].value).toBe(11);
+     expect(outcome[1].metrics[0].value).toBe(22);      
+});
+
+
+test(`parseMultipleLinesInMixedGroupAndStandAlone`, async () => {    
+    const runtime = new MdTimerRuntime();
+    const { outcome } = runtime.read(`:11\r\n-:22\r\n\r\n:33`);
     
-//     expect(timer).toBe(11 * 60);    
-// });
-
-// test(`parsedHours`, async () => {    
-//     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read("11:00:00");
-//     const timer = outcome[0].timer as number
-    
-//     expect(timer).toBe(11 * 60 * 60);    
-// });
-
-// test(`parsedDays`, async () => {    
-//     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read("11:00:00:00");
-//     const timer = outcome[0].timer as number
-//     expect(timer).toBe(11 * 60 * 60 * 24);
-// });
-
-
-// test(`parseMultipleLines`, async () => {    
-//     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read(`:11\r\n-:22`);
-
-//     expect(outcome[0].timer).toBe(11);
-//     expect(outcome[1].timer).toBe(22);    
-// });
-
-
-
-// test(`parseMultipleLinesInGroup`, async () => {    
-//     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read(`[:11\r\n-:22]`);    
-//     expect(outcome[0].timer).toBe(11);
-//     expect(outcome[1].timer).toBe(22);      
-// });
+    expect(outcome[0].metrics[0].value).toBe(11);
+    expect(outcome[1].metrics[0].value).toBe(22);    
+    expect(outcome[2].metrics[0].value).toBe(33);    
+});
 
 
 // test(`parseMultipleLinesInMixedGroupAndStandAlone`, async () => {    
 //     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read(`[\r\n:11\r\n-:22\r\n]\r\n:33`);
+//     const { outcome } = runtime.read(`[\r\n:11\r\n-:22\r\n test1,test2)\r\n:33`);
     
-//     expect(outcome[0].timer).toBe(11);
-//     expect(outcome[1].timer).toBe(22);    
-//     expect(outcome[2].timer).toBe(33);    
-// });
-
-
-// test(`parseMultipleLinesInMixedGroupAndStandAlone`, async () => {    
-//     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read(`[\r\n:11\r\n-:22\r\n](test1,test2)\r\n:33`);
-    
-//     expect(outcome[0].timer).toBe(11);
-//     expect(outcome[1].timer).toBe(22);    
-//     expect(outcome[2].timer).toBe(11);
-//     expect(outcome[3].timer).toBe(22);        
-//     expect(outcome[4].timer).toBe(33);    
+//     expect(outcome[0].metrics[0].value).toBe(11);
+//     expect(outcome[1].metrics[0].value).toBe(22);    
+//     expect(outcome[2].metrics[0].value).toBe(11);
+//     expect(outcome[3].metrics[0].value).toBe(22);        
+//     expect(outcome[4].metrics[0].value).toBe(33);    
 // });
 
 // test(`multiplierOnTimer`, async () => {    
 //     const runtime = new MdTimerRuntime();
-//     const { outcome } = runtime.read(":11(2)");        
-//     const timer = outcome[1].timer as number;
+//     const { outcome } = runtime.read("2x :11");        
+//     const repeater = outcome[0].repeater;
     
-//     expect(timer).toBe(11);    
+//     expect(repeater).not.toBeNull();    
 // });
